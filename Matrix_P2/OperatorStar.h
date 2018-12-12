dbdc39e8e706b188035d9c5c8e2e79c42a92a945
gabrieleacerbi <45733069+gabrieleacerbi@users.noreply.github.com>
2018-12-12 13:50:41 +0100

    Update OperatorStar.h

--- Matrix_P2/OperatorStar.h
+++ Matrix_P2/OperatorStar.h
@@ -18,15 +18,29 @@ public:
 	static constexpr unsigned Width = W;
 	
 	//friend mMult<T,matrix_ref<T,Left>> 
+	
+	template<class matrix_class>
+	void empl_back(matrix_ref<T, matrix_class> m){
+		this->matrix_List.emplace_back(m);
+	}
+	
 
 template<class Right>
-mMult<T,this_Type::Height,Right::Width> operator* ( const matrix_ref<T, Right>& right) {
+mMult<T,this_Type::Height,matrix_ref<T, Right>::Width> operator* (const matrix_ref<T, Right>& right) {
+	this->matrix_List.emplace_back(right); // adds right to the end of matrix_List
 	return *this;
 }
 
 };
 
-
+template<typename T, class Left, class Right>
+mMult<T, matrix_ref<T, Left>::Height, matrix_ref<T, Right>::Width> operator* (const matrix_ref<T, Left>& left, const matrix_ref<T, Right>& right){
+	mMult<T, matrix_ref<T, Left>::Height, matrix_ref<T, Right>::Width> chain;
+	chain.empl_back(left);
+	chain.empl_back(right);
+	return chain;
+}
+	
 
 
 
