715dc4a1d266b6a37737f22458cbd735e571c996
francesco.busolin <francesco.busolin@libero.it>
2018-12-12 18:41:29 +0100

    matrix chain multiplication for matrix<int,5,5> (toy) working

--- Matrix_P2/example1.cc
+++ Matrix_P2/example1.cc
@@ -1,7 +1,70 @@
 #include<iostream>
 
 #include"matrix.h"
+#include "OperatorStar.h"
+ // Matric Chain Multiplication Algorithm !
 
+ // multiply two matrices
+matrix<int, 5, 5> matMult(matrix<int, 5, 5> a, matrix<int, 5, 5> b) {
+	int r1 = 5; // TODO: change magic numbers
+	int r2 = 5;
+	int c1 = 5;
+	int c2 = 5;
+	matrix<int, 5, 5> m;
+	for (int i = 0; i < r1; ++i)
+		for (int j = 0; j < c2; ++j)
+			for (int k = 0; k < c1; ++k)
+			{
+				m(i,j) = m(i,j) + a(i,k) * b(k,j);
+			}
+	return m;
+}
+// given an optimal sequence resolve
+matrix<int, 5, 5> mult(std::vector<matrix<int, 5, 5>> A, std::vector<std::vector<int>> s,int i, int j) {
+	if (i == j) return A[i];
+	int k = s[i][j];
+	matrix<int,5,5> X = mult(A,s,i, k);
+	matrix<int,5,5> Y = mult(A, s, k + 1, j);
+	return matMult(X, Y);
+}
+// find dimensions of a list of matrices
+std::vector<int> extractDims(std::vector<matrix<int, 5, 5>> list) {
+	int n = list.size();
+	std::vector<int> res;
+	res.resize(n + 1, 0);
+	res[0] = list[0].get_height();
+	for (int i = 1; i < n; i++) {
+		res[i] = list[i].get_height();
+	}
+	res[n] = list[n - 1].get_width();
+	return res;
+}
+// find parentesis using dynamic algorithm
+matrix<int, 5, 5> doMult(std::vector<matrix<int, 5, 5>> list) {
+	matrix<int, 5, 5> result;
+	unsigned n = list.size();
+	std::vector<std::vector<int>> m, s;
+	std::vector<int> p;
+	m.resize(n, std::vector<int>(n, 0));
+	s.resize(n, std::vector<int>(n, 0));
+	p = extractDims(list);
+	for (int L = 2; L < n; L++) {
+		for (int i = 1; i < n - L + 1; i++) {
+			int j = i + L - 1;
+			m[i][j] = 999999;
+			for (int k = i; k <= j - 1; k++) {
+				int q = m[i][k] + m[k + 1][j] + p[i - 1 + 1] * p[k] * p[j];
+				if (q < m[i][j]) {
+					m[i][j] = q;
+					s[i][j] = k;
+				}
+			}
+		}
+	}
+	return mult(list,s,0,n-1);
+}
+
+// end matrix multiply
 
 int main() {
 
@@ -37,8 +100,9 @@ for (int i = 0; i != 5; ++i) {
 }
 std::cout << std::endl;
 
+std::vector<matrix<int, 5, 5>> mts{A,A,A};
+auto mults = doMult(mts);
 //auto H = A * A;
-
 /*
 auto C=B.window({1,4,1,4});
 std::cout << C(1,2) << ' ' << *(C.col_begin(2)) << ' ' << *++(C.col_begin(2)) << std::endl;
@@ -94,3 +158,5 @@ for (int i=0; i!=4; ++i) {
 std::cout << std::endl;
 return 0;*/
 }
+
+
