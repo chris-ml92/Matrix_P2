9daec86e4b46c2250825b76030ec9de0f7aef8a8
gabrieleacerbi <45733069+gabrieleacerbi@users.noreply.github.com>
2018-12-15 17:07:35 +0100

    Update OperatorPlus.h

--- Matrix_P2/OperatorPlus.h
+++ Matrix_P2/OperatorPlus.h
@@ -1,3 +1,8 @@
+// Authors
+// Christian Bernabe Cabrera (843382)
+// Francesco Busolin (851884)
+// Gabriele Acerbi (877653)
+
 #ifndef OPERATORPLUS_H
 #define OPERATORPLUS_H
 #include <type_traits>
